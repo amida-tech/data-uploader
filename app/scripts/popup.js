6f37a9d5507d3d2fb75ae4fbb7ba9cbb64cdcc1b
OEHokie <matt.martz@gmail.com>
2015-06-17 12:41:59 -0400

    added start of DRE login/submit extension stopped working though

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -2,9 +2,10 @@
 
 var dreChromeApp = angular.module('dreChromeApp', []);
 
-dreChromeApp.controller('dreCtrl', ['$scope', function ($scope) {
+dreChromeApp.controller('dreCtrl', ['$scope', 'authentication', function ($scope, authentication) {
 
 	$scope.selectedService = {};
+    $scope.dre = {};
 	$scope.tabStep = 0;
 	$scope.services = [
 		{
@@ -35,8 +36,79 @@ dreChromeApp.controller('dreCtrl', ['$scope', function ($scope) {
 		$scope.selectedService = selectService;
 		$scope.tabStep=1;
 	};
-
 	$scope.secondStep = function() {
-		$scope.tabStep = 2;
+        authentication.login($scope.dre.domain, $scope.dre.username, $scope.dre.password, function(err,data) {
+            if (err) {
+                console.log("err: ",err);
+            }
+            if (data) {
+                console.log("success? "+data);
+                $scope.tabStep = 2;
+            } else {
+                console.log("failed...");
+            }
+        });
 	};
-}]);
+}]).service('authentication', function authentication($rootScope, $location, $http) {
+        var auth_data = {};
+
+        function clearAuth() {
+            auth_data = {};
+            $rootScope.isAuthorized = false;
+        }
+        this.clearAuth = clearAuth;
+
+        this.authStatus = function (domain, callback) {
+            if (Object.keys(auth_data).length > 0) {
+                if (auth_data.authenticated) {
+                    callback(null, true);
+                } else {
+                    callback(null, false);
+                }
+            } else {
+                $http.get(domain+'/api/v1/account')
+                    .success(function (data) {
+                        if (data && data.authenticated) {
+                            auth_data.authenticated = true;
+                            callback(null, true);
+                        } else {
+                            auth_data.authenticated = false;
+                            callback(null, false);
+                        }
+
+                    }).error(function (err) {
+                        auth_data.authenticated = false;
+                        callback(err, false);
+                    });
+            }
+        };
+
+        this.login = function (domain, username, password, callback) {
+            if (domain && username && password) {
+                $http.post(domain+'/api/v1/login', {
+                        username: username,
+                        password: password
+                    })
+                    .success(function () {
+                        auth_data.authenticated = true;
+                        callback(null,true);
+                    })
+                    .error(function () {
+                        console.log("login failed");
+                        auth_data.authenticated = false;
+                        callback('Invalid Login and/or Password.',false);
+                    });
+            }
+        };
+
+        this.logout = function (domain, callback) {
+            $http.post(domain+'/api/v1/logout')
+                .success(function () {
+                    clearAuth();
+                    callback(null);
+                }).error(function (err) {
+                    console.log("logout failed");
+                    callback(err);
+                });
+        };
+    });
