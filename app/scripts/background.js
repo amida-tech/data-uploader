507a76771c1839125b43ca243033ee4c658f0d68
OEHokie <matt.martz@gmail.com>
2015-07-01 14:20:29 -0400

    Improvements Retrieves full record for premium and as-full-as-possible for basic My VA user.  Running into a problem with cookies.  Angular posts the cookie with the request which doesn't clear in a timely manner.  So if I log in with user A, even if I use user B's login/password user A's cookie gets sent and VA just looks at that.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -7,3 +7,13 @@ chrome.runtime.onInstalled.addListener(function(details) {
 //chrome.browserAction.setBadgeText({text: '\'Allo'});
 
 //console.log('\'Allo \'Allo! Event Page for Browser Action');
+
+/*
+chrome.cookies.getAll({url:'https://www.myhealth.va.gov'},function(cookiesArr) {
+    for (var i = 0; i < cookiesArr.length; i++) {
+      chrome.cookies.remove({url:cookiesArr[i].url, name:cookiesArr[i].name},function(err){
+         console.log("cookie removed!");
+      });
+    }
+});
+*/
